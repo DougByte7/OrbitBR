e64baf8d2cef200695be2aa176ced2e5e41ec53e
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2024-08-13 10:35:42 -0300

    Update topbar

--- src/components/top-bar/top-bar.tsx
+++ src/components/top-bar/top-bar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Countdown from "./components/countdown";
 import { IconBrandDiscordFilled } from "@tabler/icons-react";
@@ -10,10 +9,8 @@ interface TopBarProps {
 }
 export default async function TopBar({ children }: TopBarProps) {
   return (
-    <header className="top-0 z-10 flex w-full items-center justify-center border-b border-border/5 bg-[rgba(19,18,20,0.50)] py-4 sm:sticky sm:h-[4.375rem] sm:py-0">
+    <header className="top-0 z-10 flex w-full items-center justify-center border-b border-border/5 bg-[rgba(19,18,20,0.50)] py-4 backdrop-blur-2xl sm:sticky sm:h-[4.375rem] sm:py-0">
       <div className="flex w-full max-w-[1320px] flex-wrap justify-between gap-3 sm:gap-12">
-        <Image src="/favicon.svg" alt="Orbit BR Logo" width={36} height={36} />
-
         <div className="flex gap-3">
           <Button variant="outline" asChild>
             <a href="https://discord.gg/HbBFEj5PJ6" target="_blank">
@@ -25,8 +22,11 @@ export default async function TopBar({ children }: TopBarProps) {
           <Button
             className="gap-3 border-[rgba(255,208,44,0.25)] bg-[#1E1B17] text-[#FFD02C] hover:border-[#FFD02C]"
             variant="outline"
+            asChild
           >
-            <HandHeart size={20} /> Seja um apoiador
+            <a href="https://apoia.se/orbitstt" target="_blank">
+              <HandHeart size={20} /> Seja um apoiador
+            </a>
           </Button>
         </div>
 
