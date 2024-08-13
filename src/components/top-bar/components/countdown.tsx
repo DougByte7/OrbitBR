f7c86c92f1d3382ce65499df9dfbf931e857d600
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2024-08-13 14:37:10 -0300

    Try reset view

--- src/components/top-bar/components/countdown.tsx
+++ src/components/top-bar/components/countdown.tsx
@@ -2,30 +2,35 @@
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { addDays } from "date-fns";
+import { useGameActions } from "@/app/state";
 
 export const revalidate = 0;
 
+const computeCountdown = () => {
+  const now = new Date();
+  const nextMidDay =
+    now.getHours() >= 12
+      ? addDays(now, 1).setHours(12, 0, 0, 0)
+      : new Date().setHours(12, 0, 0, 0);
+
+  const diff = nextMidDay - now.getTime();
+  const hours = diff / 1000 / 60 / 60;
+  const minutes = (hours - (hours | 0)) * 60;
+  const seconds = (minutes - (minutes | 0)) * 60;
+  const format = (n: number) => `${n | 0}`.padStart(2, "0");
+
+  return `${format(hours)}:${format(minutes)}:${format(seconds)}`;
+};
+
 export default function Countdown() {
-  const computeCountdown = () => {
-    const now = new Date();
-    const nextMidDay =
-      now.getHours() >= 12
-        ? addDays(now, 1).setHours(12, 0, 0, 0)
-        : new Date().setHours(12, 0, 0, 0);
-
-    const diff = nextMidDay - now.getTime();
-    const hours = diff / 1000 / 60 / 60;
-    const minutes = (hours - (hours | 0)) * 60;
-    const seconds = (minutes - (minutes | 0)) * 60;
-    const format = (n: number) => `${n | 0}`.padStart(2, "0");
-
-    return `${format(hours)}:${format(minutes)}:${format(seconds)}`;
-  };
-
   const [countdown, setCountdown] = useState(computeCountdown);
+  const { resetView } = useGameActions();
 
   useEffect(() => {
-    const timeout = setTimeout(() => setCountdown(computeCountdown()), 1000);
+    const timeout = setTimeout(() => {
+      setCountdown(computeCountdown());
+      resetView();
+    }, 1000);
 
     return () => {
       clearTimeout(timeout);
