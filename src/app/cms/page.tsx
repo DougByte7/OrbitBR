03477864d7f670a10b7076eeb62bd2770d8216d0
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2024-08-13 21:13:48 -0300

    Add login to cms

--- src/app/cms/page.tsx
+++ src/app/cms/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import AnimeList from "./components/anime-list";
 import NewAnime from "./components/new-anime-button";
@@ -13,7 +13,6 @@ import {
 } from "@mantine/core";
 import TopBar from "@/components/top-bar";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import { api } from "@/trpc/server";
 import { genres, status, streamings } from "@/constants/animes";
 import { FilterIcon } from "lucide-react";
@@ -25,40 +24,40 @@ export default async function CMS({
   searchParams: Record<string, string | undefined>;
 }) {
   const user = await currentUser();
-  if (!user?.privateMetadata.admin) redirect("/");
 
   return (
     <main>
-      <SignedIn>
-        <TopBar>
+      <TopBar>
+        <SignedIn>
           <UserButton />
-        </TopBar>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton>Entrar</SignInButton>
+        </SignedOut>
+      </TopBar>
 
-        {user.privateMetadata.admin ? (
-          <div className="grid gap-4 py-6">
-            <AnimeFilters params={searchParams} />
-            <AnimeList />
+      {user?.privateMetadata.admin ? (
+        <div className="grid gap-4 py-6">
+          <AnimeFilters params={searchParams} />
+          <AnimeList />
+        </div>
+      ) : (
+        // No one should ever see this, but just in case
+        <Stack className="p-4" align="center">
+          <Image
+            src="/images/shrek.jpg"
+            alt={"shrek smith"}
+            width={500}
+            height={500}
+          />
+          <div className="text-center">
+            <div className="text-white">Tu não divia estar aqui não parça!</div>
+            <Link className="text-blue-500" href="/">
+              Voltar a área segura.
+            </Link>
           </div>
-        ) : (
-          // No one should ever see this, but just in case
-          <Stack className="p-4" align="center">
-            <Image
-              src="/images/shrek.jpg"
-              alt={"shrek smith"}
-              width={500}
-              height={500}
-            />
-            <div className="text-center">
-              <div className="text-white">
-                Tu não divia estar aqui não parça!
-              </div>
-              <Link className="text-blue-500" href="/">
-                Voltar a área segura.
-              </Link>
-            </div>
-          </Stack>
-        )}
-      </SignedIn>
+        </Stack>
+      )}
     </main>
   );
 }
